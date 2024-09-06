b4460bca2e7db01c5882ea8538c67512e246f426
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2024-09-07 00:52:18 +0530

    Recursion

---
+++ Recursion/Basic/Print 1 to N  without loop.java
@@ -0,0 +1,8 @@
+// package Recursion.Basic;
+
+// public class Print 1 to N  without loop {
+    
+// }
+// Print numbers from 1 to n without the help of loops. You only need to complete the function printNos() that takes N as parameter and prints number from 1 to N recursively.
+
+// Don't print newline, it will be added by the driver code.
